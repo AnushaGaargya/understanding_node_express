4d237bc14c55e77ff6374f59ee5cf0fd483fa638
Anusha Gaargya <anushasgarg@gmail.com>
2023-08-16 23:12:29 +0530

    understanding req res objects and rendering ejs templates

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const app = express()
 // const router = express.Router()
+app.set('view engine', 'ejs')
+
 
 
 app.get('/user', (req,res) => {
     console.log("HI")
-    res.send("<h1>Hello</h1>")
+    console.log(req.headers)
+    // res.set({
+    //     'content-type': 'text/plain',
+    //     'warning': 'encoding will be addded by default'
+    // })
+    var name = 'Anusha'
+    res.render('hello', {name: name
+       
+    })
+    // console.log(res.headersSent)
+    // console.log(res.body)
+    // console.log(req.headers)
+})
+
+app.get('/error', (req,res) => {
+    
+    res.status(500)
+    res.send('error')
 })
 
 
