eccf440423bcbe92f71ab61193999469bd251e98
Anusha Gaargya <anushasgarg@gmail.com>
2023-08-17 01:17:37 +0530

    dynamic data rendering using ejs

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }));
 
-
+const del_btns = {}
 
 app.get('/user', (req,res) => {
     console.log("HI")
-    res.render('hello')
+    res.render('hello', {
+        del_btns : del_btns
+    })
 })
 
 
@@ -16,11 +18,26 @@ app.get('/user', (req,res) => {
 app.post('/user', (req,res) => {
     console.log(req.body.name)
     console.log(req.body.email)
-    res.render('welcome', {
-        name: req.body.name
-    })
+    del_btns[req.body.id] = req.body.name
+    console.log(del_btns)
+    res.redirect('/user')
+    // res.render('welcome', {
+    //     name: req.body.name
+    // })
 
 })
+
+app.post('/deleteprocess', (req,res) => {
+    
+    console.log("HURRAY! WE ARE IN DELETE PROCESS")
+    console.log(req.body.delete)
+    const del_id = req.body.delete
+    console.log(del_btns)
+    console.log(del_btns[del_id])
+    delete del_btns[del_id]
+    res.redirect('/user')
+})
+
 
 
 app.listen(3000)
